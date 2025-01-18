085c673db2dbc6c145bbb746875847284d85135c
juanfelipegh <64973537+juanfelipegh@users.noreply.github.com>
2025-01-17 22:23:52 -0500

    Calculadora con import nuevo

---
+++ Act2/src/index.js
@@ -0,0 +1,7 @@
+import { calculadora } from "./math.js";
+
+console.log(calculadora("sumar",10,5))
+console.log(calculadora("restar",20,8))
+console.log(calculadora("multiplicar",6,7))
+console.log(calculadora("dividir",15,3))
+console.log(calculadora("dividir",15,0))
